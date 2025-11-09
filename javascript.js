2d6dc82c26a6665500e09763b4193742469ed7d5
diegonv-gh <diegonvaladares@gmail.com>
2025-11-08 22:47:14 -0300

    consertando as imagens e adicionando footer

--- javascript.js
+++ javascript.js
@@ -1,17 +1,34 @@
 const items = document.querySelectorAll(".item");
+const items2 = document.querySelectorAll(".item2");
 const previa = document.getElementById("previa");
-
-// const imagens = [
-//   {nome: "Santos - SP", img: "santos.jpeg"},
-//   {nome: "Rio de Janeiro - RJ", img: "saopaulo.jpg"},
-//   {nome: "", img: "riodejaneiro.jpg"},
-//   {nome: "Belo Horizonte - MG", img: "belohorizonte.jpg"},
-//   {nome: "Curitiba - PR", img: "curitiba.jpg"},
-// ];
+const previa2 = document.getElementById("previa2")
 
 items.forEach(e => {
   e.addEventListener("mouseenter", () => {
-    let atributo = e.getAttribute("data-img");
-    previa.src = atributo
-  })
-})
+    const atributo = e.getAttribute("data-img");
+
+    // Faz a imagem sumir
+    previa.style.opacity = 0;
+
+    // Espera a animação de sumir terminar, troca a imagem e reaparece
+    setTimeout(() => {
+      previa.src = atributo;
+      previa.style.opacity = 1;
+    }, 100); // tempo igual ao do CSS transition
+  });
+});
+
+items2.forEach(e => {
+  e.addEventListener("mouseenter", () => {
+    const atributo = e.getAttribute("data-img");
+
+    // Faz a imagem sumir
+    previa2.style.opacity = 0;
+
+    // Espera a animação de sumir terminar, troca a imagem e reaparece
+    setTimeout(() => {
+      previa2.src = atributo;
+      previa2.style.opacity = 1;
+    }, 100); // tempo igual ao do CSS transition
+  });
+});
